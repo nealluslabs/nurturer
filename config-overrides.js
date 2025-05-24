713b291ca4d18fc82c153358477e17e70708f876
nealluslabs <nealluslabs@gmail.com>
2025-05-24 12:37:06 +0100

    vm polyfill added

--- config-overrides.js
+++ config-overrides.js
@@ -38,6 +38,7 @@ module.exports = function override(config, env) {
       url: require.resolve("url"),
       path: require.resolve("path-browserify"),
       zlib: require.resolve("browserify-zlib"),
+      vm: require.resolve("vm-browserify"),
       fs: false, // Not available in browsers
     },
   };
