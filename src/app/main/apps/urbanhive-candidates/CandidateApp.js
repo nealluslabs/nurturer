86ef1ab909ee6e09cb3fdac074e8b8ddcf908e08
nealluslabs <nealluslabs@gmail.com>
2025-06-27 18:45:14 +0100

    screen temp fix

--- src/app/main/apps/urbanhive-candidates/CandidateApp.js
+++ src/app/main/apps/urbanhive-candidates/CandidateApp.js
@@ -78,7 +78,7 @@ function CandidateApp(props) {
 
 
 
-<Grid container spacing={0} style={{ display: "flex", justifyContent: "space-between" ,position:"relative",left:"65%",transform: "translateX(-50%)",top:"3rem",width:"40rem",flexDirection:"row",marginBottom:"1.5rem",zIndex:"1000"}}>
+<Grid container spacing={0} style={{ display: "flex", justifyContent: "space-between" ,position:"relative",/*left:"65%"*/left: "calc(80vw - 44%)",transform: "translateX(-50%)",top:"3rem",width:"40rem",flexDirection:"row",marginBottom:"1.5rem",zIndex:"1000"}}>
               
               {/*1*/}
                <Grid item>
