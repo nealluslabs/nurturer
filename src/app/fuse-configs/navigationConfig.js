00b7506768c57dbfed7b925c7c976efa5d84dd4c
nealluslabs <nealluslabs@gmail.com>
2025-06-27 15:49:42 +0100

    Commented some icons

--- src/app/fuse-configs/navigationConfig.js
+++ src/app/fuse-configs/navigationConfig.js
@@ -4,8 +4,8 @@ import i18next from 'i18next';
 import ar from './navigation-i18n/ar';
 import en from './navigation-i18n/en';
 import tr from './navigation-i18n/tr';
-import { IoMdCard } from "react-icons/io";
-import { FaNewspaper } from "react-icons/fa";
+// import { IoMdCard } from "react-icons/io";
+// import { FaNewspaper } from "react-icons/fa";
 
 
 i18next.addResourceBundle('en', 'navigation', en);
